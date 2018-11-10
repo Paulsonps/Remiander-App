a97550ea9b40ec12757d6d4476d3816acc39406c
deepak.s <deepak.s@neoito.com>
2018-11-10 13:18:20 +0530

    form integration

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,19 +1,30 @@
-import { Component, OnInit, AfterViewInit, ViewChild, ElementRef } from '@angular/core';
+import {
+  Component,
+  OnInit,
+  AfterViewInit,
+  ViewChild,
+  ElementRef
+} from '@angular/core';
 import { RadSideDrawer } from 'nativescript-ui-sidedrawer';
 import * as app from 'tns-core-modules/application';
 import { RadialScale, RadialBarIndicator } from 'nativescript-ui-gauge';
+import { Time } from '../model/time.model';
 
 @Component({
   selector: 'Home',
   moduleId: module.id,
   templateUrl: './home.component.html'
 })
-export class HomeComponent implements AfterViewInit {
+export class HomeComponent {
+  time: Time;
   timeLeft: number = 60;
   interval;
-  constructor() {}
-
-  ngAfterViewInit() {}
+  constructor() {
+    console.log('h1');
+    this.time = new Time();
+    this.time.setTime = '100';
+    this.time.setTask = 'My init task';
+  }
 
   startTimer() {
     this.interval = setInterval(() => {
